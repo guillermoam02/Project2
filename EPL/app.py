fa8d68a6e79ea3be40cb1a8c1b683a88750108b5
Guillermo Avila <memoavila71@hotmail.com>
2020-05-29 13:33:13 -0500

    update URI

--- EPL/app.py
+++ EPL/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:2311@127.0.0.1:5432/EPL9219"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 #engine = create_engine(f'postgresql://{app.config["SQLALCHEMY_DATABASE_URI"]}', echo=False)
 
 db.init_app(app)
